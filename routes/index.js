84bdf4949752ea8450c19545abedfd4b97216429
mmadhusoodan <mmadhusoodan>
2015-09-01 01:42:53 -0700

    fixed the great and good batsman login

--- routes/index.js
+++ routes/index.js
@@ -30,9 +30,8 @@ exports.getPlayerForCountry = function (req, res) {
 }
 
 exports.getPlayerWithBatAvg = function (req, res) {
-
-    var bat_avg = req.params.id;
-    var query = "select * from PLAYER where tests > 20 and bat_avg > " + " '" + bat_avg + "' ORDER BY runs DESC ";
+    var bat_avg = req.params.id / 10;
+    var query = "select * from PLAYER where tests > 20 and bat_avg LIKE " + " '" + bat_avg + "%' ORDER BY runs DESC ";
     db.all(query, function (err, row) {
         if (err !== null) {
             res.send(500, "An error has occurred -- " + err);
